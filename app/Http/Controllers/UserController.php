567893b5a1ec6b41d8f9c97f6cac004dbc6213c6
Manzar <manzarabbasshah1@gmail.com>
2024-07-23 20:17:26 +0500

    validation rule

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
 
     function getFormData(Request $req){
         return $req->validate([
-             'username' => 'required | min:5' ,
+             'username' => 'required | min:5 | Uppercase' ,
             'password' => 'required | min:5'
         ],[
             'username.required' => 'Username empty h'
