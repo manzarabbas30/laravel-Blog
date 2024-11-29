6df70addbfafd41eddd4a1236530f4cff7e2c4eb
Manzar <manzarabbasshah1@gmail.com>
2024-11-29 18:22:38 +0500

    API login and  Authentication with Laravel sanctum

--- app/Http/Controllers/userAuthController.php
+++ app/Http/Controllers/userAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 class userAuthController extends Controller
 {
     function Apisignup(Request $request)
@@ -13,12 +13,18 @@ class userAuthController extends Controller
         $input["password"]= bcrypt($input["password"]);
         $user= User::create($input);
         $success["token"]= $user->createToken('MyApp')->plainTextToken;
-        // $user["name"]=$user->name;
+        $success["name"]=$user->name;
         return ["success"=>true,"result"=>$success,"msg"=> "Token created Successfully"];
     }
 
     function Apilogin(Request $request)
     {
-        return 'Some code';
+        $user= User::where('email',$request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return ["result"=>"login not Found", "success"=>false];
+        }
+        $success["token"]= $user->createToken('MyApp')->plainTextToken;
+        $success["name"]=$user->name;
+        return ["success"=>true,"result"=>$success,"msg"=> "Token created Successfully"];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,32 +14,38 @@ Route::get('/test', function () {
     return ["name"=>"manzar","Job"=>"Web developer"];
 });
 
-Route::get('/list-student',[StudentController::class,'listStudentApi']);
-
-Route::post('/add-student',[StudentController::class,'addStudentApi']);
-// {
-//     "name":"saleem2 Abbas",
-//     "email":"saleem2@gmail.com",
-//     "batch":"22"
-//   }
 
 
-Route::put('/update-student',[StudentController::class,'updateStudentApi']);
-// {
-//     "id":"30",
-//      "name":"Ali Abbas",
-//      "email":"ali@gmail.com",
-//      "batch":"212"
-//   }
+Route::group(["middleware" => "auth:sanctum"],function(){
+    Route::get('/list-student',[StudentController::class,'listStudentApi']);
 
-Route::delete('/delete-student/{id}',[StudentController::class,'deleteStudentApi']);
-
-
-Route::get('/search-student/{key}',[StudentController::class,'searchStudentApi']);
-
-Route::resource('member',resourceController::class);
+    Route::post('/add-student',[StudentController::class,'addStudentApi']);
+    // {
+    //     "name":"saleem2 Abbas",
+    //     "email":"saleem2@gmail.com",
+    //     "batch":"22"
+    //   }
+    
+    
+    Route::put('/update-student',[StudentController::class,'updateStudentApi']);
+    // {
+    //     "id":"30",
+    //      "name":"Ali Abbas",
+    //      "email":"ali@gmail.com",
+    //      "batch":"212"
+    //   }
+    
+    Route::delete('/delete-student/{id}',[StudentController::class,'deleteStudentApi']);
+    
+    
+    Route::get('/search-student/{key}',[StudentController::class,'searchStudentApi']);
+    
+    Route::resource('member',resourceController::class);
+    
+});
 
 
 Route::post('/signup',[userAuthController::class,'Apisignup']);
 
 Route::post('/login',[userAuthController::class,'Apilogin']);
+Route::get('/login',[userAuthController::class,'Apilogin'])->name('login');
